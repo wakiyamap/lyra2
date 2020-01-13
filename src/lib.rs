b8af5f4df87b872e8d93d5076728f65ca2ac2ae0
Jin Eguchi <wakiyamap@gmail.com>
2020-01-13 14:08:25 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,2 @@
-pub mod lyra2 {
-	pub fn sum(_k: u64, _pwd: Vec<u8>, _salt: Vec<u8>, _time_cost: u64, _n_rows: u64, _n_cols: u64) {}
-}
-pub mod lyra2rev2 {
-	pub fn sum(_input: Vec<u8>) {}
-}
+pub mod lyra2;
+pub mod lyra2rev2;
